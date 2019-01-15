d1cc0822d9857522b95a99c3e7fb5d0067810e04
Matt Outen <mattoutenphysed@gmail.com>
2019-01-15 17:36:05 -0600

    netlify form test

--- src/components/Main.js
+++ src/components/Main.js
@@ -114,7 +114,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="POST" action="https://formspree.io/outenmp@gmail.com">
+          <form name="contact" method="POST" data-netlifty="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
