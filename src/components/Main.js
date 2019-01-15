974d96dec5bfeeef397892a0a20610abb2f1dd4c
Matt Outen <mattoutenphysed@gmail.com>
2019-01-15 17:47:15 -0600

    netlify form test UNDO

--- src/components/Main.js
+++ src/components/Main.js
@@ -114,7 +114,11 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" data-netlify="true">
+          <form
+            name="contact"
+            method="POST"
+            action="https://formspree.io/outenmp@gmail.com"
+          >
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
