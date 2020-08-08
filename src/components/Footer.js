8dbb0ba4a07d25df6f2c6c7722bc2513190f67c9
matt.outen <matt.outen@mattouten.local>
2020-08-08 11:51:56 -0400

    change adaptive height on carousel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ import PropTypes from 'prop-types'
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">
-      &copy; Created by Matt Outen. Design:{' '}
-      <a href="https://html5up.net">HTML5 UP</a>. Built with:{' '}
+      &copy; Created by Matt Outen. Built with:{' '}
       <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
     </p>
   </footer>
--- src/components/my_work/CaitlinMayConsulting.js
+++ src/components/my_work/CaitlinMayConsulting.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Layout from './template';
-import image from '../../images/CMC.png';
+import React from 'react'
+import Layout from './template'
+import image from '../../images/CMC.png'
 
-const CakedInButter = () => (
-	<Layout
-		title="Caitlin May Consulting"
-		imgSrc={image}
-		imgAlt="caitlin may consulting"
-		timeline="March 2019 - present"
-		body="Caitlin May Consulting is a professional consulting website I built for my sister."
-		bulletOne="Gatsby"
-		bulletTwo="Graphql"
-		bulletThree="React Slick/MailChimp/Stripe"
-		githubLink="https://github.com/mouten57/caits-site"
-		liveLink="https://www.caitlinmayconsulting.com"
-		readmeLink="https://github.com/mouten57/caked_in_butter/blob/master/README.md"
-	/>
-);
+const CMC = () => (
+  <Layout
+    title="Caitlin May Consulting"
+    imgSrc={image}
+    imgAlt="caitlin may consulting"
+    timeline="March 2019 - present"
+    body="Caitlin May Consulting is a professional consulting website I built for my sister."
+    bulletOne="Gatsby"
+    bulletTwo="Graphql"
+    bulletThree="React Slick/MailChimp/Stripe"
+    githubLink="https://github.com/mouten57/caits-site"
+    liveLink="https://www.caitlinmayconsulting.com"
+    readmeLink="https://github.com/mouten57/caked_in_butter/blob/master/README.md"
+  />
+)
 
-export default CakedInButter;
+export default CMC
